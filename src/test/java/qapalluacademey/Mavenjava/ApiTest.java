869be1f6df54a400c1e7b2eb62676b7cb2d28a8e
Pallavi <pallavinimbalkar10@gmail.com>
2020-11-10 15:47:03 +0530

    first commit

---
+++ src/test/java/qapalluacademey/Mavenjava/ApiTest.java
@@ -0,0 +1,22 @@
+package qapalluacademey.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+
+@Test
+public void jira()
+{
+	System.out.println("jira");
+	}
+
+@Test
+public void twitter()
+{
+	System.out.println("twitter");
+
+	
+	
+}
+
+}
---
+++ src/test/java/qapalluacademey/Mavenjava/AppiumTest.java
@@ -0,0 +1,23 @@
+package qapalluacademey.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+
+	@Test
+	public void Nativeappandroid()
+	{
+		System.out.println("Nativeappandroid");
+		
+		}
+
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+		
+	}
+
+
+}
